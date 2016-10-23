7ce0f1806be554d283f2f330ecc45abc081b0598
zhangxiaoping <xiaoping6783@163.com>
2016-10-23 18:40:54 +0800

    just

--- src/com/ping/base/EqualsTest.java
+++ src/com/ping/base/EqualsTest.java
@@ -1,7 +1,14 @@
 package com.ping.base;
 
 public class EqualsTest {
-	
+
+	/**
+	 *
+	 */
+	public void test01(){
+		System.out.print("hello world");
+	}
+
 	public static void main(String[] args){
 		String a = "hello";
 		String b = "hello";
