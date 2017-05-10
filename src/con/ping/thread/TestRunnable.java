7577d87373fa71e26bd636ad6a94e20ece0dd7bb
zhangxiaoping <xiaoping6783@163.com>
2017-05-10 17:23:27 +0800

    update

--- src/con/ping/thread/TestRunnable.java
+++ src/con/ping/thread/TestRunnable.java
@@ -3,5 +3,15 @@ package con.ping.thread;
 /**
  * Created by zhangxiaoping on 17/5/4.
  */
-public class TestRunnable {
+public class TestRunnable implements Runnable {
+
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getId() + " : " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args){
+        new Thread(new TestRunnable()).start();
+    }
 }
