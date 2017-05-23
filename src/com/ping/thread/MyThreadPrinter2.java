2c2a6d18e8be8801a08764076a49bd213f4a5248
zhangxiaoping <xiaoping6783@163.com>
2017-05-23 16:58:46 +0800

    threadpool

--- src/com/ping/thread/MyThreadPrinter2.java
+++ src/com/ping/thread/MyThreadPrinter2.java
@@ -52,15 +52,15 @@ public class MyThreadPrinter2 implements Runnable {
         Object c = new Object();   
         MyThreadPrinter2 pa = new MyThreadPrinter2("A", c, a);   
         MyThreadPrinter2 pb = new MyThreadPrinter2("B", a, b);   
-        MyThreadPrinter2 pc = new MyThreadPrinter2("C", b, c);   
-           
-           
+        MyThreadPrinter2 pc = new MyThreadPrinter2("C", b, c);
+
+
         new Thread(pa).start();
         Thread.sleep(100);
         new Thread(pb).start();
         Thread.sleep(100);
         new Thread(pc).start();
         Thread.sleep(100);
-    }   
+    }
 }  
 
--- src/com/ping/thread/ThreadCancle.java
+++ src/com/ping/thread/ThreadCancle.java
@@ -1,7 +1,29 @@
 package com.ping.thread;
 
+import com.ping.thread.future.MyThread;
+
 /**
  * Created by zhangxiaoping on 17/4/15.
  */
 public class ThreadCancle {
+
+
+    public static void main(String[] args){
+
+        Thread thread = new MyTask();
+
+        thread.start();
+
+
+    }
+
+
+    static class MyTask extends Thread {
+
+        @Override
+        public  void run(){
+            System.out.println();
+        }
+    }
+
 }
