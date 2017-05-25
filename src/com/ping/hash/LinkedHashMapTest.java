d74adb2c38e98a71852f1cbeb4cae4f88df78f6b
zhangxiaoping <xiaoping6783@163.com>
2017-05-25 20:57:19 +0800

    addLin

--- src/com/ping/hash/LinkedHashMapTest.java
+++ src/com/ping/hash/LinkedHashMapTest.java
@@ -1,7 +1,25 @@
 package com.ping.hash;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by zhangxiaoping on 17/5/4.
  */
 public class LinkedHashMapTest {
+
+    public void testMap(){
+
+        LinkedHashMap<String,String> hashMap = new LinkedHashMap<String, String>();
+
+
+
+        hashMap.put("a","name");
+        hashMap.get("a");
+    }
+
+
+    public static void main(String[] args){
+
+    }
+
 }
